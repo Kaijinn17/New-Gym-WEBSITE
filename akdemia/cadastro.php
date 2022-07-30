07bdac29a836d7242511f25df709065cb356bc37
Whoami <79420054+Kaijinn17@users.noreply.github.com>
2022-07-29 23:32:44 -0300

    Add files via upload

---
+++ akdemia/cadastro.php
@@ -0,0 +1,26 @@
+<?php include_once"config.php"; ?>
+
+<?php
+
+$nome = $_POST['name'];
+$telefone = $_POST['phone'];
+$email = $_POST['email'];
+$senha = $_POST['password'];
+$data_nascimento = $_POST['date'];
+$genero = $_POST['select'];
+
+$sql = "INSERT INTO clientes(nome,telefone,email,senha,data_nascimento,genero)VALUES('$nome','$telefone','$email','$senha','$data_nascimento','$genero') ";
+
+if (mysqli_query($conn, $sql)){
+
+     header('Location: Login.html');
+
+}else{
+
+     echo "Deu erro" . $sql . "<br>" . mysqli_error($conn);
+
+}
+
+mysqli_close($conn);
+
+?>
