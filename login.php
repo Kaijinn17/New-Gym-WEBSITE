2bdfc0fd01fa52c57f5c6b817b1652414cf2ac76
Whoami <79420054+Kaijinn17@users.noreply.github.com>
2022-08-15 20:47:57 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -2,7 +2,21 @@
 
 include('config.php');
 
-$mysqli = new mysqli("ec2-54-85-56-210.compute-1.amazonaws.com/", "vivuuvngcdbszv", "8b0e2945b9a7a493843837d2fd2c22585136ce20f859ee51517857780f47ebf1", "dfopd816p36hgh");
+$servidor = "ec2-54-85-56-210.compute-1.amazonaws.com/";
+//Aqui é o nome de usuário do seu banco de dados, root é o servidor inicial e
+//básico de todo servidor, mas recomenda-se não usar o usuario root e sim criar um novo usuário
+$usuario = "vivuuvngcdbszv";
+//Aqui colocamos a senha do usuário, por padrão o usuário root vem sem senha,
+//mas é altamente recomendável criar uma senha para o usuário root, visto que ele é
+//o que tem mais privilégios no servidor
+$senha ="8b0e2945b9a7a493843837d2fd2c22585136ce20f859ee51517857780f47ebf1";
+
+//Aqui criamos a conexão utilizando o servidor, usuario e senha,
+//caso dê erro, retorna um erro ao usuário.
+$conexao = pg_connect($servidor, $usuario, $senha) or
+die ("Não foi possível conectar ao servidor PostGreSQL");
+//caso a conexão seja efetuada com sucesso, exibe uma mensagem ao usuário
+echo "Conexão efetuada com sucesso!!";
 
 
 
