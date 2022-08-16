7ed83bd078e89d1afd51aa4b5059ab0c4e3048f5
Whoami <79420054+Kaijinn17@users.noreply.github.com>
2022-08-15 21:05:54 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,15 +1,19 @@
 <?php
+$endereco = 'ec2-54-85-56-210.compute-1.amazonaws.com';
+$banco = 'dfopd816p36hgh';
+$usuario = 'vivuuvngcdbszv';
+$senha = '8b0e2945b9a7a493843837d2fd2c22585136ce20f859ee51517857780f47ebf1';
 
-public Connection getConnectionPostgreSQL() {
-    try{
-        return DriverManager.getConnection(
-            "jdbc:postgresql://"
-            +"ec2-54-85-56-210.compute-1.amazonaws.com/"
-            +"dfopd816p36hgh",
-            "vivuuvngcdbszv",
-            "8b0e2945b9a7a493843837d2fd2c22585136ce20f859ee51517857780f47ebf1");
-    } catch (SQLException e) {
-        throw new RuntimeException(e);
-    }
+try {
+    $pdo = new PDO("pgsql:host=$endereco;port=5432;dbname$banco", $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+
+    echo "Conectado no Banco de dados";
+} catch (PDOException $e) {
+
+  echo "Falha ao conectar. <br/>";
+  die($e->getMessage());
+
 }
+
 ?>
--- login.php
+++ login.php
@@ -2,23 +2,7 @@
 
 include('config.php');
 
-$servidor = "ec2-54-85-56-210.compute-1.amazonaws.com/";
-//Aqui é o nome de usuário do seu banco de dados, root é o servidor inicial e
-//básico de todo servidor, mas recomenda-se não usar o usuario root e sim criar um novo usuário
-$usuario = "vivuuvngcdbszv";
-//Aqui colocamos a senha do usuário, por padrão o usuário root vem sem senha,
-//mas é altamente recomendável criar uma senha para o usuário root, visto que ele é
-//o que tem mais privilégios no servidor
-$senha ="8b0e2945b9a7a493843837d2fd2c22585136ce20f859ee51517857780f47ebf1";
-
-//Aqui criamos a conexão utilizando o servidor, usuario e senha,
-//caso dê erro, retorna um erro ao usuário.
-$conexao = pg_connect($servidor, $usuario, $senha) or
-die ("Não foi possível conectar ao servidor PostGreSQL");
-//caso a conexão seja efetuada com sucesso, exibe uma mensagem ao usuário
-echo "Conexão efetuada com sucesso!!";
-
-
+$pdo = new PDO("pgsql:host=$endereco;port=5432;dbname$banco", $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
 if(isset($_POST['email']) || isset($_POST['senha'])) {
 
