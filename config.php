2b39b6a1dc51bbf43a30c75e997b7efd831054cf
Whoami <79420054+Kaijinn17@users.noreply.github.com>
2022-08-15 21:26:34 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,19 +1,17 @@
 <?php
-$endereco = 'ec2-54-85-56-210.compute-1.amazonaws.com';
-$banco = 'dfopd816p36hgh';
-$usuario = 'vivuuvngcdbszv';
-$senha = '8b0e2945b9a7a493843837d2fd2c22585136ce20f859ee51517857780f47ebf1';
+$server = 'ec2-54-85-56-210.compute-1.amazonaws.com';
+$user = 'vivuuvngcdbszv';
+$password = '8b0e2945b9a7a493843837d2fd2c22585136ce20f859ee51517857780f47ebf1';
+$port = "5432";
 
-try {
-    $pdo = new PDO("pgsql:host=$endereco;port=5432;dbname$banco", $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+$infos_string = "host=$server port=$port dbname=postgres user=$user password=$password";
 
+$conection = pg_connect($infos_string) or
+die ("Não foi possível conectar ao servidor");
+echo "Conexão efetuada com Sucesso";
 
-    echo "Conectado no Banco de dados";
-} catch (PDOException $e) {
-
-  echo "Falha ao conectar. <br/>";
-  die($e->getMessage());
-
-}
+pg_close($conection) or
+die ("Não foi possível desconectar");
+echo "<br>Desconectado com sucesso";
 
 ?>
