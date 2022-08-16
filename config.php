de972b2748818def1c63173fbc962387e6961a2a
Whoami <79420054+Kaijinn17@users.noreply.github.com>
2022-08-16 12:09:40 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,13 @@
 <?php
-$server = 'ec2-54-85-56-210.compute-1.amazonaws.com';
-$user = 'vivuuvngcdbszv';
-$password = '8b0e2945b9a7a493843837d2fd2c22585136ce20f859ee51517857780f47ebf1';
-$port = "5432";
 
-$infos_string = "host=$server port=$port dbname=postgres user=$user password=$password";
 
-$conection = pg_connect($infos_string) or
-die ("Não foi possível conectar ao servidor");
-echo "Conexão efetuada com Sucesso";
 
-pg_close($conection) or
-die ("Não foi possível desconectar");
-echo "<br>Desconectado com sucesso";
+$serverApi = new ServerApi(ServerApi::V1);
+
+$client = new MongoDB\Client(
+
+    'mongodb+srv://whoami:akdemiacetep@cluster0.gzba77m.mongodb.net/users?retryWrites=true&w=majority', [], ['serverApi' => $serverApi]);
+
+$db = $client->test;
 
 ?>
