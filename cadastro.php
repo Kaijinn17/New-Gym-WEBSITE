952899a8d5eeef11f2c0744f8b27520a92becced
Whoami <79420054+Kaijinn17@users.noreply.github.com>
2022-08-16 12:18:03 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -24,6 +24,66 @@ else{
 
 }
 
+class Person implements MongoDB\BSON\Persistable
+
+{
+
+    private $id;
+
+    private $name;
+
+    private $createdAt;
+
+    public function __construct($name)
+
+    {
+
+        $this->id = new MongoDB\BSON\ObjectId;
+
+        $this->name = (string) $name;
+
+        $this->createdAt = new MongoDB\BSON\UTCDateTime;
+
+    }
+
+    function bsonSerialize()
+
+    {
+
+        return [
+
+            '_id' => $this->id,
+
+            'name' => $this->name,
+
+            'createdAt' => $this->createdAt,
+
+        ];
+
+    }
+
+    function bsonUnserialize(array $data)
+
+    {
+
+        $this->id = $data['_id'];
+
+        $this->name = $data['name'];
+
+        $this->createdAt = $data['createdAt'];
+
+    }
+
+}
+
+$collection = (new MongoDB\Client)->test->persons;
+
+$result = $collection->insertOne(new Person('Bob'));
+
+$person = $collection->findOne(['_id' => $result->getInsertedId()]);
+
+var_dump($person);
+
 
 mysqli_close($conn);
 
