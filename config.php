59d89b3ec27bddd4f6f5d292e3430167e0925412
Whoami <79420054+Kaijinn17@users.noreply.github.com>
2022-08-19 10:29:56 -0300

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $usuario = 'root';
 $senha = '';
 
 try {
-  $pdo = new PDO("mysql:host=$endereco;port=5432;charset=utf8;dbname=$banco", $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+  $pdo = new PDO("mysql:host=$endereco;port=3306;charset=utf8;dbname=$banco", $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $e) {
   die($e->getMessage());
 }
