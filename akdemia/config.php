c3cf84e4386c679b6a31609fa42ff2cdb454ccbd
Whoami <79420054+Kaijinn17@users.noreply.github.com>
2022-08-19 19:52:06 -0300

    Add files via upload

---
+++ akdemia/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$endereco = '127.0.0.1';
+$banco = 'akdemia';
+$usuario = 'whoami';
+$senha = 'akdemiacetep';
+
+try {
+  $pdo = new PDO("mysql:host=$endereco;port=3307;charset=utf8;dbname=$banco", $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (PDOException $e) {
+  die($e->getMessage());
+}
