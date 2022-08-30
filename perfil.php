53b79539a263ff8db628b4cc4fb969fd948bf541
Kaijinn17 <gaaraplax@gmail.com>
2022-08-30 20:36:57 -0300

    up

--- perfil.php
+++ perfil.php
@@ -176,7 +176,7 @@
                             <h5 class="card-title">Periodo</h5>
                             <p class="card-text">Segunda a sabado.</p>
                             <p class="card-text d-flex justify-content-center">
-                                <?php if ($planos = 'Plano Trimestral'): ?>
+                                <?php if ($user->planos = $user->planos): ?>
                                     <p>plano selecionado</p>
                                     </p>
                             <?php else : ?>
