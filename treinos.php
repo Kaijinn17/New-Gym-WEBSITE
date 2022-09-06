e85627799e2e62b7dc34ee755fa774aebfcec707
Kaijinn17 <gaaraplax@gmail.com>
2022-09-06 16:18:45 -0300

    up

--- treinos.php
+++ treinos.php
@@ -88,8 +88,8 @@
     </nav>
   </header>
 
-  <div class="text-bg-light p-1 d-lg-flex justify-content-center" style="font-size: 16px;">
-    <strong>Olá<h3 style="color: red;"><?php if (isset($_SESSION['nome'])) echo $_SESSION['nome'];?></h3>Continue Em Forma Com Treinos Em Casa</strong>
+  <div class="text-bg-light p-2 d-lg-flex justify-content-center" style="font-size: 16px;">
+    <strong>Olá <p style="color: red;"><?php if (isset($_SESSION['nome'])) echo $_SESSION['nome'];?></p> Continue Em Forma Com Treinos Em Casa</strong>
 </div>
  
   <div class="cards d-flex justify-content-center m-4">
