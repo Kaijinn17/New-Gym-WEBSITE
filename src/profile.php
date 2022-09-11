6871a1840d0bcb13cbebef2b9582fa2482344a8c
Kaijinn17 <gaaraplax@gmail.com>
2022-09-11 15:51:36 -0300

    up

--- src/profile.php
+++ src/profile.php
@@ -4,9 +4,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once "/cadastro.php";
 require_once __DIR__ . '/createTable.php';
 require_once(__DIR__ . "/../config.php");
-require_once "/cadastro.php";
+
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
