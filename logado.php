7e79084f2094bf8a9c4df9621482c6fe4b4a1370
Kaijinn17 <gaaraplax@gmail.com>
2022-10-24 20:34:47 -0300

    add

--- logado.php
+++ logado.php
@@ -91,7 +91,7 @@
             <li class="nav-item">
               <a class="nav-link" href="./perfil.php">Perfil</a>
             </li>
-            <?php if($email == 'adm@gmail.com') : ?>
+            <?php if($user->$_POST['email'] == 'adm@gmail.com') : ?>
               <li class="nav-item">
               <a class="nav-link" href="./solicitacoes.php">Solicitações</a>
             </li>
