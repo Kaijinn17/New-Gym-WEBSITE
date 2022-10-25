749518b44b4d765e0f1f1ef82d92f6656ed0ed2a
Kaijinn17 <gaaraplax@gmail.com>
2022-10-25 03:14:35 -0300

    add

--- src/aprovar.php
+++ src/aprovar.php
@@ -2,10 +2,14 @@
 require_once('./config.php');
 
 $id = $_GET['id'];
-$plano = $_GET['plano'];
+$planos = $_GET['plano'];
 
-$sql = "UPDATE clientes SET planos = $plano WHERE id = $id";
+$sql = "UPDATE clientes SET planos = $planos WHERE id = $id";
 $sql = $connect->query($sql);
 $sql = "UPDATE solicitacoes SET aprovado = 1 WHERE id = $id";
 $sql = $connect->query($sql);
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(":planos", $_GET['plano']);
+$stmt->bindValue(":id", $_SESSION['id']);
+$stmt->execute();
 header("location: ../solicitacoes.php");
