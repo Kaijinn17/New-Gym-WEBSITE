6a3d68a790f65eade767a9e84f281e590b11f133
Kaijinn17 <gaaraplax@gmail.com>
2022-10-25 02:48:27 -0300

    add

--- src/aprovar.php
+++ src/aprovar.php
@@ -6,14 +6,12 @@ $planos = $_GET['plano'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['plano'])) {
     try {
-        $sql = "UPDATE clientes SET planos = :planos WHERE id = :id";
+        $sql = "UPDATE clientes SET planos = $planos WHERE id = $id";
         $stmt = $pdo->prepare($sql);
-        $sql = "UPDATE solicitacoes SET aprovado = 1 WHERE id = :id";
+        $sql = "UPDATE solicitacoes SET aprovado = 1 WHERE id = $id";
         $sql = $connect->query($sql);
-        if ($stmt->rowCount() > 0) {
             header("location: ./solicitacoes.php");
             exit;
-        }
     } catch (PDOException $err) {
         echo "Error: " . $err->getMessage();
         exit;
