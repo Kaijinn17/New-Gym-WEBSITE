c188c4260bd52e4ce83805238f40e7bdf3b8bf91
Kaijinn17 <gaaraplax@gmail.com>
2022-10-25 03:59:05 -0300

    add

--- src/aprovar.php
+++ src/aprovar.php
@@ -10,9 +10,14 @@ $plano = $_GET['plano'];
 
 $sql = "UPDATE clientes SET planos = '$plano' WHERE id = $id";
 $stmt = $pdo->prepare($sql);
+$stmt->bindValue(":planos", $_GET['plano']);
+$stmt->bindValue(":id", $_SESSION['id']);
+$stmt->execute();
 
 $sql = "UPDATE solicitacoes SET aprovado = 1 WHERE id = $id";
-$stmt = $pdo->prepare($sql);
+$stmt->bindValue(":aprovado", $_GET['aprovado']);
+$stmt->bindValue(":id", $_SESSION['id']);
+$stmt->execute();
 
 header("location: ../solicitacoes.php");
 
