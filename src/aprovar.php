34efbdfc8cd3a578fc9dac68db34c0f373718996
Kaijinn17 <gaaraplax@gmail.com>
2022-10-25 02:42:05 -0300

    add

--- src/aprovar.php
+++ src/aprovar.php
@@ -1,22 +1,24 @@
 <?php
-    require_once('./config.php');
+require_once('./config.php');
 
-    $id = $_GET['id'];
-    $planos = $_GET['plano'];
+$id = $_GET['id'];
+$planos = $_GET['plano'];
 
-    if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['plano'])) {
-        try {
-            $sql = "UPDATE clientes SET planos = :planos WHERE id = :id";
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindValue(":planos", $_GET['plano']);
-            $stmt->bindValue(":id", $_SESSION['id']);
-            $stmt->execute();
-            if ($stmt->rowCount() > 0) {
-                header("location: ../perfil.php");
-                exit;
-            }
-} catch (PDOException $err) {
-echo "Error: " . $err->getMessage();
-exit;
-}
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['plano'])) {
+    try {
+        $sql = "UPDATE clientes SET planos = :planos WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(":planos", $_GET['plano']);
+        $stmt->bindValue(":id", $_SESSION['id']);
+        $stmt->execute();
+        $sql = "UPDATE solicitacoes SET aprovado = 1 WHERE id = :id";
+        $sql = $connect->query($sql);
+        if ($stmt->rowCount() > 0) {
+            header("location: ../solicitacoes.php");
+            exit;
+        }
+    } catch (PDOException $err) {
+        echo "Error: " . $err->getMessage();
+        exit;
+    }
 }
