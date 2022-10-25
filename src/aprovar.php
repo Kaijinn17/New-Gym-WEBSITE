b0c5ef8fa0481e09423a3335676e73a5a49d368e
Kaijinn17 <gaaraplax@gmail.com>
2022-10-25 03:47:29 -0300

    add

--- src/aprovar.php
+++ src/aprovar.php
@@ -5,7 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$d = $_GET['id'];
+$id = $_GET['id'];
 $plano = $_GET['plano'];
 
 $sql = "UPDATE clientes SET planos = '$plano' WHERE id = $id";
@@ -15,3 +15,5 @@ $sql = "UPDATE solicitacoes SET aprovado = 1 WHERE id = $id";
 $sql = $pdo->query($sql);
 
 header("location: ../solicitacoes.php");
+
+?>
