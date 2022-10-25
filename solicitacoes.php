f429abe71f71afdb9435743ee3abba01c9674bf7
Kaijinn17 <gaaraplax@gmail.com>
2022-10-25 00:34:32 -0300

    add

--- solicitacoes.php
+++ solicitacoes.php
@@ -110,13 +110,13 @@
      echo "LISTA DE SOLICITAÇÕES";
 
      if($sql->rowCount() > 0){
-    $tabela = '<table border="1" style="display:flex;background-color:white;">';//abre table
+    $tabela = '<table border="1" style="background-color:white;">';//abre table
     $tabela .='<thead>';//abre cabeçalho
     $tabela .= '<tr>';//abre uma linha
-    $tabela .= '<th style="padding-right:5px;">ID</th>'; // colunas do cabeçalho
-    $tabela .= '<th style="padding-right:5px;>Nome</th>';
-    $tabela .= '<th style="padding-right:5px;>Plano_Antigo</th>';
-    $tabela .= '<th style="padding-right:5px;>Plano_Novo</th>';
+    $tabela .= '<th style="margin-right:5px;">ID</th>'; // colunas do cabeçalho
+    $tabela .= '<th style="margin-right:5px;>Nome</th>';
+    $tabela .= '<th style="margin-right:5px;>Plano_Antigo</th>';
+    $tabela .= '<th style="margin-right:5px;>Plano_Novo</th>';
     $tabela .= '<th>Número de Aprovação</th>';
     $tabela .= '</tr>';//fecha linha
     $tabela .='</thead>'; //fecha cabeçalho
