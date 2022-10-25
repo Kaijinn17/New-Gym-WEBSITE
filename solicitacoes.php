7b865d09d98677167c8eba5765898ffa0fd44dcb
Kaijinn17 <gaaraplax@gmail.com>
2022-10-25 01:40:22 -0300

    add

--- solicitacoes.php
+++ solicitacoes.php
@@ -134,12 +134,12 @@
     $tabela .='</tbody>'; //fecha corpo
     $tabela .= '</table>';//fecha tabela
   }else{
-    echo "<br><br><br>";
-    echo "NENHUMA SOLICITAÇÃO ENCONTRADA";
+    $messagi .= '<h1>NENHUMA SOLICITAÇÃO ENCONTRADA</h1>';
 }
 echo '<td><a style="color:black;background-color:yellow;margin-right:30px;" href="./src/profile.php?id='.$dado['usuario_id'].'&plano='.$dado['plano_novo'].'">APROVAR</a><a style="color:black;background-color:yellow;margin-right:30px;" href="./src/profile.php?id='.$dado['id'].'">REPROVAR</a></td>';  
             echo "</tr>";
     echo $tabela; // imprime
+    echo $mesagi;
     
 
     
