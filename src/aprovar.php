3d24386cac673246fc66accc77b38b30c6864e93
Kaijinn17 <gaaraplax@gmail.com>
2022-10-25 03:32:14 -0300

    add

--- src/aprovar.php
+++ src/aprovar.php
@@ -4,8 +4,12 @@ require_once('./config.php');
 $id = $_GET['id'];
 $planos = $_GET['plano'];
 
-$sql = "UPDATE clientes SET planos = $planos WHERE id = $id";
+$sql = "UPDATE clientes SET planos = '$planos' WHERE id = $id";
 $sql = $connect->query($sql);
+
 $sql = "UPDATE solicitacoes SET aprovado = 1 WHERE id = $id";
 $sql = $connect->query($sql);
+
 header("Location: ../solicitacoes.php");
+
+?>
