270727c45b7880fffd008dccf5ecaacb0d2eee8a
Kaijinn17 <gaaraplax@gmail.com>
2022-10-25 03:39:11 -0300

    add

--- src/aprovar.php
+++ src/aprovar.php
@@ -1,15 +1,27 @@
 <?php
 require_once('./config.php');
 
-$id = $_GET['id'];
-$planos = $_GET['plano'];
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['plano'])) {
+    try {
 $sql = "UPDATE clientes SET planos = '$planos' WHERE id = $id";
 $sql = $connect->query($sql);
 
 $sql = "UPDATE solicitacoes SET aprovado = 1 WHERE id = $id";
 $sql = $connect->query($sql);
 
-header("Location: ../solicitacoes.php");
-
-?>
+$stmt->bindValue(":planos", $_GET['plano']);
+        $stmt->bindValue(":id", $_SESSION['id']);
+        $stmt->execute();
+if ($stmt->rowCount() > 0) {
+    header("Location: ../solicitacoes.php");
+    exit;
+}
+} catch (PDOException $err) {
+echo "Error: " . $err->getMessage();
+exit;
+}
+}
