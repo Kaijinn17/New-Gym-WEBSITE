979fcb7aaf5bd1d8082dd32e39700e1fc50e092b
Kaijinn17 <gaaraplax@gmail.com>
2022-10-27 04:15:16 -0300

    add

--- src/planotri.php
+++ src/planotri.php
@@ -13,12 +13,10 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 
-$planos = $_GET['planos'];
-$nome = $_GET['nome'];
-$res->bindparam(":nome", $nome);
-$res->bindparam(":planos", $planos);
+$planos = $_POST['planos'];
+$nome = $_POST['nome'];
 
-$sql = "INSERT INTO solicitacoes (nome, plano_antigo, plano_novo, aprovado) VALUES (':nome', ':planos', 'trimestral', 0)";
+$sql = "INSERT INTO solicitacoes (nome, plano_antigo, plano_novo, aprovado) VALUES ('$nome', '$planos', 'trimestral', 0)";
 $sql = $pdo->query($sql);
 
 header("Location: ../perfil.php");
