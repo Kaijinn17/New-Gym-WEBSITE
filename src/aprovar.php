f5bb198468d02bb3159c0f14f3ab51942992a7ae
Kaijinn17 <gaaraplax@gmail.com>
2022-10-26 23:16:34 -0300

    add

--- src/aprovar.php
+++ src/aprovar.php
@@ -4,19 +4,22 @@ require_once(__DIR__ . "/../config.php");
 
 if (isset($_POST['update'])) {
 
-    $id = $_GET['id'];
-    $plano = $_GET['plano'];
+    try {
 
-    $sqlUpdate = "UPDATE clientes SET planos = '$plano' WHERE id = '$id'";
-    $stmt = $pdo->prepare($sqlUpdate);
-    $stmt->bindValue(":planos", $_GET['plano']);
-    $sqlAprovado = "UPDATE solicitacoes SET aprovado = '1' WHERE id = '$id'";
-    $stmt = $pdo->prepare($sqlAprovado);
-    $stmt->execute();
-    if ($stmt->rowCount() > 0) {
-        header("Location: ./solicitacoes.php");
+        $id = $_GET['id'];
+        $plano = $_GET['plano'];
+
+        $sqlUpdate = "UPDATE clientes SET planos = '$plano' WHERE id = '$id'";
+        $stmt = $pdo->prepare($sqlUpdate);
+        $sqlAprovado = "UPDATE solicitacoes SET aprovado = '1' WHERE id = '$id'";
+        $stmt = $pdo->prepare($sqlAprovado);
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) {
+            header("Location: ./solicitacoes.php");
+            exit;
+        }
+    } catch (PDOException $err) {
+        echo "Error: " . $err->getMessage();
         exit;
     }
 }
-
-
