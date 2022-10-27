1e9e218393e065eeaa606113bfe2ae920629474a
Kaijinn17 <gaaraplax@gmail.com>
2022-10-27 03:58:15 -0300

    add

--- src/planotri.php
+++ src/planotri.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/createTable.php';
 require_once(__DIR__ . "/../config.php");
-include_once ("./perfil.php");
+require_once("./profile.php");
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
