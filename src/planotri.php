32ac0bbce394630cd89d8a5f2ed5abf0358d8d66
Kaijinn17 <gaaraplax@gmail.com>
2022-10-27 19:18:26 -0300

    add

--- src/planotri.php
+++ src/planotri.php
@@ -11,9 +11,6 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$sql = "SELECT * FROM clientes";
-$sql = $pdo->query($sql);
-
 $id = $_GET["id"];
 $planos = $_GET["planos"];
 $nome = $_GET["nome"];
