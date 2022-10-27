21da4a4c1842e84e8e9e00a2290e5388290c41d0
Whoami <79420054+Kaijinn17@users.noreply.github.com>
2022-10-27 12:39:49 -0300

    add

--- src/planotri.php
+++ src/planotri.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/createTable.php';
 require_once(__DIR__ . "/../config.php");
 
-require_once("./profile.php");
+
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
