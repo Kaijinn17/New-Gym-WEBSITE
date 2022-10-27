209ac2e410ec980a5859220020b0aa3a28199b4d
Whoami <79420054+Kaijinn17@users.noreply.github.com>
2022-10-27 12:56:09 -0300

    Add

--- src/planotri.php
+++ src/planotri.php
@@ -17,8 +17,8 @@ if (session_status() === PHP_SESSION_NONE) {
 
 
 
-$planos = $_GET["planos"];
-$nome = $_GET["nome"];
+$planos = $_GET['planos'];
+$nome = $_GET['nome'];
 
 $sql = "INSERT INTO solicitacoes (nome, plano_antigo, plano_novo, aprovado) VALUES ('$nome', '$planos', 'trimestral', 0)";
 $sql = $pdo->query($sql);
