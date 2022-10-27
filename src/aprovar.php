68a0f4213e8b0adaa4e2dd3a13187b89421dbbfe
Kaijinn17 <gaaraplax@gmail.com>
2022-10-26 23:13:48 -0300

    add

--- src/aprovar.php
+++ src/aprovar.php
@@ -8,10 +8,15 @@ if (isset($_POST['update'])) {
     $plano = $_GET['plano'];
 
     $sqlUpdate = "UPDATE clientes SET planos = '$plano' WHERE id = '$id'";
-    $result = $pdo->query($sqlUpdate);
-
+    $stmt = $pdo->prepare($sqlUpdate);
+    $stmt->bindValue(":planos", $_GET['plano']);
     $sqlAprovado = "UPDATE solicitacoes SET aprovado = '1' WHERE id = '$id'";
-    $result = $pdo->query($sqlAprovado);
+    $stmt = $pdo->prepare($sqlAprovado);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        header("Location: ./solicitacoes.php");
+        exit;
+    }
 }
 
-header("Location: ./solicitacoes.php");
+
