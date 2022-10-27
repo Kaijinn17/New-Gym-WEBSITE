bfc03460522a82cdf4c95d993cbc51fce645a7a2
Kaijinn17 <gaaraplax@gmail.com>
2022-10-27 02:51:18 -0300

    add

--- perfil.php
+++ perfil.php
@@ -272,7 +272,7 @@
                             <p class="card-text d-flex justify-content-center">
                                 <?php
 
-                                echo "<a href='./src/planotri.php?id="['id'].'&plano_antigo='['planos'].'&nome='['nome']. "' class='w-100'>
+                                echo "<a href='./src/planotri.php?plano_antigo='.['$planos'].'&nome='.['$nome'].' class='w-100'>
                                         Adquirir
                                 </a>"
                                 ?>
