0e0932212030ba9b87c0cdf1ce6f270521933035
Kaijinn17 <gaaraplax@gmail.com>
2022-10-26 23:30:36 -0300

    add

--- src/aprovar.php
+++ src/aprovar.php
@@ -12,7 +12,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['plano'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_POST['plano'])) {
     try {
 
         $id = $_POST['id'];
@@ -20,13 +20,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['plano'])) {
 
         $sqlUpdate = "UPDATE clientes SET planos = $planos WHERE id = $id";
         $stmt = $pdo->prepare($sqlUpdate);
-        $stmt->execute();
         $sqlAprovado = "UPDATE solicitacoes SET aprovado = 1 WHERE id = $id";
         $stmt = $pdo->prepare($sqlAprovado);
         $stmt->execute();
         if ($stmt->rowCount() > 0) {
-
-
             header("Location: ../solicitacoes.php");
             exit;
         }
