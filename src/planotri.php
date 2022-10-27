f862c8066cde2e24ae4a6de6135a048556ad54ef
Kaijinn17 <gaaraplax@gmail.com>
2022-10-27 01:33:15 -0300

    add

--- src/planotri.php
+++ src/planotri.php
@@ -16,7 +16,7 @@ $id = $_GET['id'];
 $nome = $_GET['nome'];
 $planos = $_GET['planos'];
 
-$sql = "INSERT INTO solicitacoes (id, nome, plano_antigo, plano_novo, aprovado) SELECT clientes.$id, solicitacoes.$nome, solicitacoes.$planos, solicitacoes.'trimestral', solicitacoes.0 FROM solicitacoes";
+$sql = "INSERT INTO solicitacoes (id, nome, plano_antigo, plano_novo, aprovado) VALUES ('$id', '$nome', '$planos', 'trimestral', '0')";
 $sql = $pdo->query($sql);
 
 header("Location: ../perfil.php");
