158f395bff38ed16542a3cb0b16ee3a8d51acc5f
Kaijinn17 <gaaraplax@gmail.com>
2022-10-28 10:34:42 -0300

    add

--- src/protect.php
+++ src/protect.php
@@ -3,5 +3,5 @@ if (session_status() === PHP_SESSION_NONE) {
   session_start();
 }
 if (!isset($_SESSION['id'])) {
-  die("Você precisa logar em uma conta para acessar está Página.<p><a href=\"logar.php\">Entrar </p>");
+  die("Você precisa logar em uma conta para acessar está Página.<p><a href=\"logar.php\">Entrar </a></p>");
 }
