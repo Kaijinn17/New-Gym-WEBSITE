05d88c28cc844245f30ef05b0c15e66fb0123685
Kaijinn17 <gaaraplax@gmail.com>
2022-10-28 10:26:54 -0300

    add

--- perfil.php
+++ perfil.php
@@ -1,5 +1,5 @@
 <?php include('./src/protect.php'); ?> 
-<?php require_once(__DIR__ . './src/profile.php'); ?> 
+<?php require_once(__DIR__ . '/src/profile.php'); ?> 
 <?php include("./config.php"); ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
